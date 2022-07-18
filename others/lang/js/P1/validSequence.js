4fedc045684d06acde1fcf7d3bfc214f7635f23d
Shakil Ahmmed <shakilfci461@gmail.com>
2022-07-19 00:54:03 +0600

    19th July 2022

--- others/lang/js/P1/validSequence.js
+++ others/lang/js/P1/validSequence.js
@@ -1,3 +1,15 @@
+/**
+ *
+ *
+ *   A subsequence of an array is a set of numbers that aren't necessarily adjacent
+ *   in the array but that are in the same order as they appear in the array. For
+ *   instance, the numbers [1, 3, 4]  form a subsequence of the array [1, 2, 3, 4] , and so do the numbers [2, 4]
+ *   . Note that a single number in an array and the array itself are both valid
+ *   subsequences of the array.
+ */
+
+
+
 function validSequence(array, sequence) {
     let arrIdx = 0;
     let seqIdx = 0;
